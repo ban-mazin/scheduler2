ef77359d41ac43bc6830246df2a9e76c5e11aec3
ban-mazin <ban.qaddoori89@gmail.com>
2022-09-22 12:04:12 -0600

    add classnames to daylistitem

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,10 +1,18 @@
 import React from "react";
+import "components/DayListItem.scss";
+
+import classnames from "classnames";
 
 export default function DayListItem(props) {
+  
+  let dayClass = classnames("DayListItem", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full":  !(props.spots) 
+  });
   return (
-    <li onClick={() => props.setDay(props.name)}>
-      <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots} spots remaining</h3>
-    </li>
-  );
+    <li className={dayClass} onClick={() => props.setDay(props.name)}>
+    <h2 className='text--regular'>{props.name}</h2>
+    <h3 className='text--light'>{props.spots} spots remining</h3>
+  </li>
+);
 }
